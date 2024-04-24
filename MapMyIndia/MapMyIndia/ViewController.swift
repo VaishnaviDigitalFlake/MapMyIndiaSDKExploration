b23a5a58f6c0dedca4697b8e60edca014caabc27
Dhanshree DF <167843970+VaishnaviDigitalFlake@users.noreply.github.com>
2024-04-24 11:13:56 +0530

    initial commit

---
+++ MapMyIndia/MapMyIndia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapMyIndia
+//
+//  Created by Digitalflake Mahesh K on 24/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
